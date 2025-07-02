bc3f2ba9316b83550676d2ec10e26d079557128b
siyam7 <muhammedsiyam7@gmail.com>
2025-07-02 20:29:12 +0530

    style status

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import "./App.css";
 export default function App() {
   return (
     <main>
       <header>
         <h2>Assembly: Endgame</h2>
-        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        <p>
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
       </header>
+      <section className="status">
+        <h2>You Win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
     </main>
   );
 }
