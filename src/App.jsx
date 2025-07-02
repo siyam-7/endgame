5450a5b80f95c043e381444ab12eeb7cde347ba3
siyam7 <muhammedsiyam7@gmail.com>
2025-07-02 19:37:00 +0530

    initial commit with header section

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+export default function App() {
+  return (
+    <main>
+      <header>
+        <h2>Assembly: Endgame</h2>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+    </main>
+  );
+}
